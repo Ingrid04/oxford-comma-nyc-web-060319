ffa38a2bf541b0271baa31621b943ed5959b7acf
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-04 21:26:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
+def oxford_comma(array)
+  
+   return array[0..-2].join(', ') + " and " + array[-1]
 end
+
+  return array[0..-4].join(", ") + " and " + array[-1]
+end 
